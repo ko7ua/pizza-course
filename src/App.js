faf54ebb6d9e421d9ef32e572e1769f9546bc2f4
ko7ua <94176894+ko7ua@users.noreply.github.com>
2022-07-15 20:18:06 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+	return (
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 className="content__title">Все пиццы</h2>
+					<div className="content__items">
+            <PizzaBlock 
+              title="С огурцами"
+              price={340}
+            />
+            <PizzaBlock 
+              title="Чизбургер-пицца"
+              price={400}
+            />
+            <PizzaBlock 
+              title="Другая пицца"
+              price={300}
+            />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+export default function Categories() {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">Все</li>
+				<li>Мясные</li>
+				<li>Вегетарианская</li>
+				<li>Гриль</li>
+				<li>Острые</li>
+				<li>Закрытые</li>
+			</ul>
+		</div>
+	);
+}
